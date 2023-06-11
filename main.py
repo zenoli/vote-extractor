4d9c2ee488eeaa1880f7de9852d4607d216ffdf7
olivier <olibitter@gmail.com>
2023-06-11 11:36:55 +0200

    Implement 'followed_recommendation' stat

--- main.py
+++ main.py
@@ -1,12 +1,49 @@
 import camelot
+import requests
 import pandas as pd
-from json import loads, dumps
+from json import loads, dumps, load
+from operator import itemgetter
 
-tables = camelot.read_pdf('pdfs/test.pdf')
-tables.export('foo.csv', f='csv', compress=True)
-votings_df = pd.concat(map(lambda table: table.df, tables[:-1])).reset_index(drop=True)
-print(votings_df)
+def extract_data(url):
+    print(">>>", url)
+    tables = camelot.read_pdf(url)
+    votings_df = (pd.concat( map(lambda table: table.df, tables[:-1]))
+        .rename(
+            columns={
+                0: "name",
+                1: "vote",
+                2: "group",
+                3: "canton"
+            }
+        )
+        .set_index("name")
+    )
 
-result = votings_df.to_json(orient="index")
-parsed = loads(result)
-print(dumps(parsed, indent=4))
+    print(votings_df.head())
+    return votings_df
+
+def get_url(business_reference, vote=51):
+    BASE_ROUTE = f"https://www.parlament.ch/poly/Abstimmung/{vote}/out"
+    return f"{BASE_ROUTE}/vote_{vote}_{business_reference}.pdf"
+
+
+def get_matches(entry):
+    business, recommendation = itemgetter('business', 'recommendation')(entry)
+    voting_df = extract_data(get_url(business))
+    return voting_df.vote == recommendation
+
+
+with open("recommendations.json") as f:
+    recommendations = load(f)
+
+    result_df = pd.DataFrame(data={
+        entry["business"]: get_matches(entry) for entry in recommendations
+    })
+    result_df["followed_recommendation"] = result_df.sum(axis=1)
+    print(result_df.head())
+
+
+
+
+
+# votings_df.to_json('out/test.json', orient="index", indent=2)
