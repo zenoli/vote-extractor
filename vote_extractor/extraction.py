d6557140a067cb142eca1104def5d83bfbbb2d4b
olivier <olibitter@gmail.com>
2023-11-11 20:25:31 +0100

    Prepare release 0.1.2

--- vote_extractor/extraction.py
+++ vote_extractor/extraction.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 def extract_data(url):
-    tables = camelot.read_pdf(url)
+    tables = camelot.read_pdf(url, backend="ghostscript")
     votings_df = (
         pd.concat(map(lambda table: table.df, tables[:-1]))
         .rename(columns={0: "firstname", 1: "lastname", 2: "vote", 3: "canton"})
