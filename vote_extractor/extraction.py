4d22ae5884d36fcad23029a3e4ab00bf50c75c7e
olivier <olibitter@gmail.com>
2023-11-11 15:53:06 +0100

    Add out dir creation and logs

--- vote_extractor/extraction.py
+++ vote_extractor/extraction.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 def extract_data(url):
     tables = camelot.read_pdf(url)
-    votings_df = pd.concat(map(lambda table: table.df, tables[:-1])).rename(
-        columns={0: "firstname", 1: "lastname", 2: "vote", 3: "canton"}
+    votings_df = (
+        pd.concat(map(lambda table: table.df, tables[:-1]))
+        .rename(columns={0: "firstname", 1: "lastname", 2: "vote", 3: "canton"})
+        .sort_values("firstname")
     )
     return votings_df
