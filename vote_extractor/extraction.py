8b305f0810fd5d30cf9f7cf73a748af491d15cc3
olivier <olibitter@gmail.com>
2023-11-11 21:19:35 +0100

    Remove empty lines from df

--- vote_extractor/extraction.py
+++ vote_extractor/extraction.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 def extract_data(url):
     tables = camelot.read_pdf(url, backend="ghostscript")
-    votings_df = (
-        pd.concat(map(lambda table: table.df, tables[:-1]))
-        .rename(columns={0: "firstname", 1: "lastname", 2: "vote", 3: "canton"})
-        .sort_values("firstname")
+    df = pd.concat(map(lambda table: table.df, tables[:-1])).rename(
+        columns={0: "firstname", 1: "lastname", 2: "vote", 3: "canton"}
     )
-    return votings_df
+    df["firstname"] = df["firstname"].map(lambda name: name.replace("\n", ""))
+    df = df.sort_values("firstname")
+    df = df[df["vote"] != ""]
+    return df
