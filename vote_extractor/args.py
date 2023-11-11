5c713b0becf3541db769a762bae953f586349ae5
olivier <olibitter@gmail.com>
2023-11-11 13:32:44 +0100

    Move arg parsing to separate module

---
+++ vote_extractor/args.py
@@ -0,0 +1,21 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pdf_url", help="url to the pdf to extract votes from")
+    parser.add_argument(
+        "-x",
+        "--excel",
+        help="store extracted votes as excel file",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-c", "--csv", help="store extracted votes CSV file", action="store_true"
+    )
+    parser.add_argument(
+        "-n",
+        "--name",
+        help="name of the generated file(s). Defaults to the input PDF name",
+    )
+    return parser.parse_args()
